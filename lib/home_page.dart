d767b28938b8ea02b0c2666e7d961b23000bff9c
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-01-26 23:03:34 +0530

    Finished home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String message = "";
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+
+    if (arguments != null) {
+      Map? pushArguments = arguments as Map;
+
+      setState(() {
+        message = pushArguments["message"];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text("Push message: $message"),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_push_notifications/empty_page.dart';
+import 'package:flutter_push_notifications/home_page.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +16,37 @@ void main() async {
     print("getToken : $value");
   });
 
+  FirebaseMessaging.onMessageOpenedApp.listen(
+    (RemoteMessage message) async {
+      print("onMessageOpenedApp: $message");
+      Navigator.pushNamed(
+        navigatorKey.currentState!.context,
+        '/push-page',
+        arguments: {"message", json.encode(message.data)},
+      );
+    },
+  );
+
+  FirebaseMessaging.instance.getInitialMessage().then(
+    (RemoteMessage? message) {
+      if (message != null) {
+        Navigator.pushNamed(
+          navigatorKey.currentState!.context,
+          '/push-page',
+          arguments: {"message", json.encode(message.data)},
+        );
+      }
+    },
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("_firebaseMessagingBackgroundHandler: $message");
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +57,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: EmptyPage(),
+      navigatorKey: navigatorKey,
+      routes: {
+        '/': ((context) => const EmptyPage()),
+        '/push-page': ((context) => const HomePage()),
+      },
     );
   }
 }
